81e2b00ba1b02af320aca53934e003535648bfbe
alan <alanarostegui@hotmail.com>
2022-01-08 15:17:29 -0600

    feat(marketing): assembling the app component

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./App";
 
 // Mount function to start up the app
 const mount = (htmlElement) => {
-  ReactDOM.render(<h1>Hi there,</h1>, htmlElement);
+  ReactDOM.render(<App />, htmlElement);
 };
 
 // If we are in development and isolation, then
