93a3b2ab4a11c27e12053df4b731be528d7f3ee5
alan <alanarostegui@hotmail.com>
2022-06-15 16:51:59 -0500

    feat: integration of the container and marketing

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
-const commonConfig = require('./webpack.common')
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
 
 const devConfig = {
   mode: "development",
@@ -11,10 +12,16 @@ const devConfig = {
     },
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        marketing: "marketing@http://localhost:8081/remoteEntry.js",
+      },
+    }),
     new HTMLWebpackPlugin({
       template: "./public/index.html",
     }),
   ],
 };
 
-module.exports = merge(commonConfig, devConfig)
+module.exports = merge(commonConfig, devConfig);
--- container/src/App.js
+++ container/src/App.js
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from "react";
+import { mount } from "marketing/MarketingApp";
+
+console.log(mount)
 
 const App = () => {
-    return (
-        <div>
-            Hi
-        </div>
-    )
-}
+  return <div>Hi</div>;
+};
 
-export default App
+export default App;
